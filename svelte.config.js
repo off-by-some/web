7db65c4d7128a0f3c96e95860bfe0555069eebc6
Cassidy Bridges <cassidybridges@gmail.com>
2026-07-25 19:03:52 -0600

    Further performance updates

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,7 @@ const config = {
       fallback: '404.html',
       strict: false,
     }),
-    inlineStyleThreshold: 0,
+    inlineStyleThreshold: 150000,
     paths: {
       base: process.env.NODE_ENV === 'production' ? '/web' : '',
     },
